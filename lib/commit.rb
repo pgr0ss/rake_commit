9275a0ad5bec1fef92424b3c9e77c2e37d749ae0
Paul Gross <pgross@gmail.com>
2010-06-10 20:15:39 -0500

    n/a - Remove dead code.

--- lib/commit.rb
+++ lib/commit.rb
@@ -59,13 +59,6 @@ def ok_to_check_in?
   cruise_status.pass? ? true : are_you_sure?( "Build FAILURES: #{cruise_status.failures.join(', ')}" )
 end
 
-def git_commit_with_message
-  commit_message = CommitMessage.new
-  sh_with_output("git config user.name #{commit_message.pair.inspect}")
-  message = "#{commit_message.feature} - #{commit_message.message}"
-  sh_with_output("git commit -m #{message.inspect}")
-end
-
 def are_you_sure?(message)
   puts "\n", message
   input = ""
@@ -73,12 +66,4 @@ def are_you_sure?(message)
     input = Readline.readline("Are you sure you want to check in? (y/n): ")
   end
   return input.strip.downcase[0,1] == "y"
-end
-
-def sh_with_output(command)
-  puts command
-  output = `#{command}`
-  puts output
-  raise unless $?.success?
-  output
 end
