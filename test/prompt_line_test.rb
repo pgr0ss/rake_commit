6c048fc45b64aecf8b6c0f27d53d8e84df1766ed
bchhun <himself@brianchhun.org>
2013-05-18 14:37:23 -0500

    Update PromptLine tests to use PromptHistory

--- test/prompt_line_test.rb
+++ test/prompt_line_test.rb
@@ -3,14 +3,13 @@ require File.expand_path(File.dirname(__FILE__) + "/test_helper")
 class PromptLineTest < Test::Unit::TestCase
 
   def test_message_puts_newline_if_saved_attribute_does_not_exist
-    File.expects(:exists?).with(Dir.tmpdir + "/pair.data").returns(false)
+    RakeCommit::PromptHistory.any_instance.expects(:last).returns(nil)
     RakeCommit::PromptLine.any_instance.expects(:puts).with("\n")
     assert_equal "pair: ", RakeCommit::PromptLine.new("pair").message
   end
 
   def test_message_puts_saved_attribute_if_exists
-    File.expects(:exists?).with(Dir.tmpdir + "/pair.data").returns(true)
-    File.expects(:read).with(Dir.tmpdir + "/pair.data").returns("John Doe\n")
+    RakeCommit::PromptHistory.any_instance.expects(:last).returns("John Doe")
     RakeCommit::PromptLine.any_instance.expects(:puts).with("\nprevious pair: John Doe\n")
     assert_equal "pair: ", RakeCommit::PromptLine.new("pair").message
   end
