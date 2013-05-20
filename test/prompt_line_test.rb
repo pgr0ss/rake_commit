71fe31f12b9b7ea3e1927336f6c61c595f5240c3
bchhun <himself@brianchhun.org>
2013-05-19 20:57:27 -0500

    Use readline history to provide prompt history

--- test/prompt_line_test.rb
+++ test/prompt_line_test.rb
@@ -8,20 +8,29 @@ class PromptLineTest < Test::Unit::TestCase
     assert_equal "pair: ", RakeCommit::PromptLine.new("pair").message
   end
 
-  def test_message_puts_saved_attribute_if_exists
+  def test_message_puts_last_saved_attribute_if_exists
     File.expects(:exists?).with(Dir.tmpdir + "/pair.data").returns(true)
-    File.expects(:read).with(Dir.tmpdir + "/pair.data").returns("John Doe")
+    File.expects(:read).with(Dir.tmpdir + "/pair.data").returns("Jane Doe\nJohn Doe")
     RakeCommit::PromptLine.any_instance.expects(:puts).with("\nprevious pair: John Doe\n")
     assert_equal "pair: ", RakeCommit::PromptLine.new("pair").message
   end
 
-  def test_save_will_save_entered_value_to_disk
+  def test_save_history_will_save_entered_value_to_disk_with_no_history
+    File.expects(:exists?).with(Dir.tmpdir + "/feature.data").returns(false)
     File.expects(:open).with(Dir.tmpdir + "/feature.data", "w").yields(file = mock)
-    file.expects(:write).with("card 100")
-    RakeCommit::PromptLine.new("feature").save("card 100")
+    file.expects(:puts).with(["card 100"])
+    RakeCommit::PromptLine.new("feature").save_history("card 100")
   end
 
-  def test_skips_prompt_if_attribute_is_in_exlusions
+  def test_save_history_will_save_entered_value_to_disk_with_existing_history
+    File.expects(:exists?).with(Dir.tmpdir + "/feature.data").returns(true)
+    File.expects(:read).with(Dir.tmpdir + "/feature.data").returns("card 90")
+    File.expects(:open).with(Dir.tmpdir + "/feature.data", "w").yields(file = mock)
+    file.expects(:puts).with(["card 90", "card 100"])
+    RakeCommit::PromptLine.new("feature").save_history("card 100")
+  end
+
+  def test_skips_prompt_if_attribute_is_in_exclusions
     prompt_line = RakeCommit::PromptLine.new("pair", ["pair"])
     assert_equal nil, prompt_line.prompt
   end
